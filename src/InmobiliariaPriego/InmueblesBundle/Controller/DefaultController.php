53c99ad6f9278f06224008ec9f0f46548ac01548
ruprisa <ruprisa@hotmail.com>
2015-10-19 17:31:58 +0200

    Tema 4
    - Implementación de framework Twitter Bootstrap
    - Añadir páginas:
    - Base
    - Header
    - Destacados (home)
    - FichaInmueble
    - Creación de entidad inmueble
    - Creación de parámetro y variable global de Twig para la dirección de
    contacto

--- src/InmobiliariaPriego/InmueblesBundle/Controller/DefaultController.php
+++ src/InmobiliariaPriego/InmueblesBundle/Controller/DefaultController.php
@@ -6,8 +6,32 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
-    {
-        return $this->render('InmobiliariaPriegoInmueblesBundle:Default:index.html.twig', array('name' => $name));
+    /*
+     * Obtención de los artículos relacionados
+     */
+    public function indexAction()
+    {   
+        $repository = $this->getDoctrine()
+                ->getRepository('InmobiliariaPriegoInmueblesBundle:Inmueble');
+        $destacados = $repository->findByDestacado(true);
+        
+        return $this->render('InmobiliariaPriegoInmueblesBundle:Default:Destacados.html.twig', array(
+            'destacados' => $destacados
+        ));
     }
+    
+    /*
+     * Obtención del inmueble solicitado
+     */
+    public function sheetAction($ref)
+    {
+        $repository = $this->getDoctrine()
+                ->getRepository('InmobiliariaPriegoInmueblesBundle:Inmueble');
+        $inmuebles = $repository->findByRef($ref);
+        
+        return $this->render('InmobiliariaPriegoInmueblesBundle:Default:FichaInmueble.html.twig', array(
+            'inmuebles' => $inmuebles
+        ));
+    }
+    
 }
